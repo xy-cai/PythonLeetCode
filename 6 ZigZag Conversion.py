a1140e84d8bd98b81d002fe7b1bd801a283bece4
XingyangCai <tifosi.cai@gmail.com>
2015-06-01 19:29:23 +0800

    first not complete python is too hard for me.

---
+++ 6 ZigZag Conversion.py
@@ -0,0 +1,16 @@
+# Author Xingyang Cai
+# Date 20150601
+
+class Solution:
+    # @param {string} s
+    # @param {integer} numRows
+    # @return {string}
+    def convert(self, s, numRows):
+    	for i in range(numRows):
+    		print i
+
+        
+
+if __name__ == '__main__':
+	solver = Solution()
+	solver.convert("PAHNAPLSIIGYIR", 3)
